597dd5f32b83f8aa1d9d851e9860c0eaf6fd6a19
Vivek Lokhande <lvivek260@gmail.com>
2024-04-18 17:37:42 +0530

    Application SetUp Complete

--- PlatePalace/PlatePalace/Constant/Roles.swift
+++ PlatePalace/PlatePalace/Constant/Roles.swift
@@ -18,16 +18,18 @@ enum Roles : String, Codable{
     func getViewController() -> UIViewController? {
         
         let viewController: UIViewController? = {
-            switch self{
-            case .student:
-                return AppStoryboard.student.viewController(viewControllerClass: ViewController.self)
-            case .manager:
-                return AppStoryboard.manager.viewController(viewControllerClass: ViewController.self)
-            case .trainer:
-                return AppStoryboard.trainer.viewController(viewControllerClass: ViewController.self)
-            case .principal:
-                return AppStoryboard.principal.viewController(viewControllerClass: ViewController.self)
-            }
+//            switch self{
+//            case .student:
+//                return AppStoryboard.student.viewController(viewControllerClass: ViewController.self)
+//            case .manager:
+//                return AppStoryboard.manager.viewController(viewControllerClass: ViewController.self)
+//            case .trainer:
+//                return AppStoryboard.trainer.viewController(viewControllerClass: ViewController.self)
+//            case .principal:
+//                return AppStoryboard.principal.viewController(viewControllerClass: ViewController.self)
+//            }
+            
+            UIViewController()
         }()
        return viewController
     }
--- PlatePalace/PlatePalace/Screens/SecondVC.swift
+++ PlatePalace/PlatePalace/Screens/SecondVC.swift
@@ -7,23 +7,33 @@
 
 import UIKit
 
+class MessListCell: UITableViewCell{
+    
+}
+
 class SecondVC: UIViewController {
 
+    @IBOutlet weak var messList: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension SecondVC: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = messList.dequeueReusableCell(withIdentifier: MessListCell.id, for: indexPath) as! MessListCell
+        return cell
+    }
+}
 
+
+extension SecondVC: UITableViewDelegate{
+    
 }
